88b3a727ada80f4b671f7a4bb55ed54f9b732253
koogawa <koogawa.app@gmail.com>
2016-04-30 19:26:07 +0900

    Add files

---
+++ LocationPickerController/LocationPickerController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocationPickerController
+//
+//  Created by koogawa on 2016/04/30.
+//  Copyright © 2016年 koogawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
