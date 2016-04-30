be6c55fef2de2e1fa1c5ba666d70fd9d787ae9ed
koogawa <koogawa.app@gmail.com>
2016-05-01 08:39:48 +0900

    Implement Storyboard

---
+++ LocationPickerController/LocationPickerController/LocationPickerController.swift
@@ -0,0 +1,35 @@
+//
+//  LocationPickerController.swift
+//  LocationPickerController
+//
+//  Created by koogawa on 2016/05/01.
+//  Copyright © 2016年 koogawa. All rights reserved.
+//
+
+import UIKit
+
+class LocationPickerController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- LocationPickerController/LocationPickerController/ViewController.swift
+++ LocationPickerController/LocationPickerController/ViewController.swift
@@ -3,12 +3,14 @@
 //  LocationPickerController
 //
 //  Created by koogawa on 2016/04/30.
-//  Copyright © 2016年 koogawa. All rights reserved.
+//  Copyright © 2016 koogawa. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+
+    @IBOutlet weak var locationLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func didTapSelectLocationButton(sender: AnyObject) {
+        print(1)
+    }
 
 }
 
